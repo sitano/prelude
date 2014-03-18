5f1f5241b57130a98544c37fc60af194659342e9
John.Koepi <john.koepi@gmail.com>
2014-03-18 20:57:25 +0300

    personal

---
+++ personal/personal-prog.el
@@ -0,0 +1,33 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+(prelude-require-packages '(smartparens yasnippet auto-complete flymake flycheck))
+
+(require 'smartparens)
+(require 'yasnippet)
+
+;; YASnippet magic!
+(yas-reload-all)
+
+(defun personal-prog-mode-defaults ()
+  "Personal default coding hook."
+  ;; For God's sake indent when I press return.
+  (electric-indent-mode +1)
+  (yas-minor-mode)
+  (auto-complete-mode)
+  ;; Turn off spell checking while coding; It's annoying.
+  ;; (flyspell-mode -1)
+  (setq sp-autoescape-string-quote nil)
+  ;; Add numbers to syntax highlighting
+  (font-lock-add-keywords nil
+                          '(("\\<\\([0-9]+\\(eE[+-]?[0-9]*\\)?\\|0[xX][0-9a-fA-F]+\\)\\>" . font-lock-constant-face))))
+
+(add-hook 'prelude-prog-mode-hook 'personal-prog-mode-defaults t)
+
+(define-key yas-minor-mode-map [(tab)] nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd "M-/") 'yas-expand)
+
+(provide 'personal-prog)
+;;; personal-prog.el ends here
---
+++ personal/personal.el
@@ -0,0 +1,88 @@
+;;; package --- Summary
+;;; Commentary:
+;;; - https://github.com/lunaryorn/stante-pede/blob/master/Cask
+;;; - https://github.com/rejeep/emacs
+;;; Code:
+
+;; Basic libraries
+(prelude-require-packages '(dash s f epl))
+
+;; Basic libraries - Fix environment
+(prelude-require-packages '(exec-path-from-shell))
+
+;; Allow arrow keys
+(setq guru-warn-only t)
+
+;; Default encoding
+(setq set-buffer-file-coding-system 'unix)
+
+;; No toolbar and scrollbar
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+;; Parens
+(show-paren-mode 1)
+
+;; Show when lines exceed fill column
+(prelude-require-packages '(whitespace))
+(setq whitespace-line-column 120)
+
+;; Show line numbers
+(prelude-require-packages '(nlinum))
+(global-nlinum-mode 1)
+
+;; Windows numbers
+(prelude-require-packages '(window-numbering))
+;; (window-number-mode 1)
+
+;; No backup files
+(setq make-backup-files nil)
+
+;; Save place in closed files
+(setq save-place-file "~/.emacs.d/saveplace")
+(setq-default save-place t)
+(prelude-require-packages '(saveplace))
+
+;; Move cursor to the last available pos on scrollint
+(setq scroll-error-top-bottom t)
+
+;; Handle .gz files
+(auto-compression-mode t)
+
+;; Provide templates for new files
+(auto-insert-mode t)
+
+;; Color theme
+(prelude-require-packages '(solarized-theme zenburn-theme))
+(load-theme 'solarized-dark t)
+
+(setq inhibit-startup-screen t)
+
+;; Make org mode look nicer
+(defvar org-startup-indented)
+
+;; Switching windows
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+
+;; Terminator style window splitting
+(global-set-key (kbd "C-S-o") 'split-window-below)
+(global-set-key (kbd "C-S-e") 'split-window-right)
+
+;; C-SPC is easier than M-/
+;; (global-set-key (kbd "C-SPC") 'hippie-expand)
+
+;; Enable Projectile
+(projectile-global-mode)
+
+;; Remove whitespace on save
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; Set custom ujelly theme
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/ujelly-theme")
+;; (load-theme 'ujelly t)
+
+(provide 'personal)
+;;; personal.el ends here
